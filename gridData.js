eb09645b4ef1e76a11ab3b765a66fdf2951476ff
Leo Au-Yeung <leo297747@gmail.com>
2019-02-10 01:45:20 -0500

    Create gridData.js
    Create initial gridData.js with 2d array and mutator/accessor for data

---
+++ gridData.js
@@ -0,0 +1,21 @@
+// Represents the data of the 2048 grid
+class gridData {
+
+    constructor() {
+        // data - 4x4 2d array; prepopulate with 0's by default
+        this.data = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+    }
+
+    // Mutators
+    setData(newData) { return this.data = newData; }
+
+    // Accessors
+    getData() { return this.data; }
+
+}
+// End gridData.js
