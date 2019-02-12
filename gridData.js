4c50a5de7d0bfa0b157a05bac79bbcf8166f5cb8
Leo Au-Yeung <leo297747@gmail.com>
2019-02-12 10:55:13 -0500

    Added toString, equals
    Added toString and equals functions
    toString(): Parses through this.data and creates a string version of 4x4 2d array (in an array format)
    equals(): Compares two GridData objects' toString() methods to check if they're equal

--- gridData.js
+++ gridData.js
@@ -12,10 +12,31 @@ class GridData {
     }
 
     // Mutators
-    setData(newData) { return this.data = newData; }
+    setData(newData) { this.data = newData; }
 
     // Accessors
     getData() { return this.data; }
+
+    // toString: returns string version of this.data (in array format)
+    toString() {
+        var res = "";
+        for (let i = 0; i < this.data.length; i++) {
+            res += "[";
+            for (let j = 0; j < this.data[i].length; j++) {
+                res += this.data[i][j].toString() + ", ";
+            }
+            res = res.substring(0, res.length-2);
+            res += "],";
+        }
+        res = res.substring(0, res.length-1);
+        return res;
+    }
+
+    // equals: returns whether toString() of current GridData and comparable GridData are equal
+    equals(otherGridData) {
+        return toString() == otherGridData.toString();
+    }
+
 
 }
 // End gridData.js
