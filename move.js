a61cae6a0031b0e8cc6cc3b6a6d8ada3239cd610
Leo Au-Yeung <leo297747@gmail.com>
2019-02-12 14:46:32 -0500

    Added toString(), equals()
    Added toString and equals functions to move.js
    toString() returns this.direction in a string form (though it is already a string as of now -> does same thing as getDirection)
    equals returns whether toString() of current Move equals toString() of a comparable Move

--- move.js
+++ move.js
@@ -11,11 +11,21 @@ class Move {
     setDirection(newDirection) {
         this.direction = newDirection;
         console.log("Direction is set to " + this.direction);
-        return this.direction;
     }
 
     // Accessors
     getDirection() { return this.direction; }
+
+    // toString: returns string version of this.direction
+    // (this.direction is already a string, so does same thing as getDirection as of right now)
+    toString() {
+        return this.direction.toString();
+    }
+
+    // equals: returns whether toString() of current Move and comparable Move are equal
+    equals(otherMove) {
+        return toString() == otherMove.toString();
+    }
 
 }
 // End Move.js
