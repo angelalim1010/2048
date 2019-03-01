7ce92acf1cb4089cbe8eda37034bd0da9e235a23
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-03-01 18:19:36 -0500

    removing comments

--- inputHandler.js
+++ inputHandler.js
@@ -1,6 +1,7 @@
 //class that listens to keyboard inputs
 class InputHandler {
-    //parameter is the callback function to take in move object
+    //parameter is the callback function to take in string
+    //callback is a function that takes in a string -> void
     constructor(callback) {
       this.callback = callback;
       const keyDown = this._keyDown.bind(this);
@@ -12,22 +13,18 @@ class InputHandler {
      const keyCode = event.key;
       switch(keyCode){
         case "ArrowUp":
-        //will set the direction of move to up
           this.callback("UP");
           console.log("the up arrow was pressed");
           break;
         case "ArrowDown":
-        //will set the direction of move to down
           this.callback("DOWN");
           console.log("the down arrow was pressed");
           break;
         case "ArrowRight":
-        //will set the direction of move to right
           this.callback("RIGHT");
           console.log("the right arrow was pressed");
         break;
         case "ArrowLeft":
-        //will set the direction of move to left
           this.callback("LEFT");
           console.log("the left arrow was pressed");
         break;
