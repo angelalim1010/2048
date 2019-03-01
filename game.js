d2661bd7dbe57e40c1f44b2c587a383840d0a0d2
Leo <leo297747@gmail.com>
2019-03-01 14:33:08 -0500

    Added inputHandler to game.js

--- game.js
+++ game.js
@@ -2,12 +2,14 @@ class Game {
     constructor() {
         this.gridData = new GridData();
         this.gridDisplay = document.getElementById('grid-container');
+        this.inputHandler = new InputHandler();
     }
 
     // Starts a new game from the beginning
     play() {
         // render 2048 Grid as a table inside the 'grid-container' element
         this.gridDisplay.innerHTML = this.gridData.renderGrid();
+        this.inputHandler;
     }
 
 }
