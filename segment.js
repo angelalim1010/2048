f43c883ba119b994f0e186d6fad74aabb6e9f9e9
Leo <leo297747@gmail.com>
2019-03-01 20:15:08 -0500

    Adding planned files for displayGridData & segment

---
+++ segment.js
@@ -0,0 +1,29 @@
+
+class Segment {
+
+    constructor(fourNumbers) {
+
+    }
+
+    // toString: returns string version of this.data (in array format)
+    toString() {
+        
+    }
+
+    // equals: returns whether toString() of current GridData and comparable GridData are equal
+    equals(otherGridData) {
+        
+    }
+    
+    // returns a segment shifted to the left
+    compactLeft() {
+
+    }
+
+    // returns a segment REVERSED
+    reverse() {
+
+    }
+
+}
+// End gridData.js
