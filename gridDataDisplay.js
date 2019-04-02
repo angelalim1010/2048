b077a3efc984360b1b3716402b33dffd67d6fe41
Leo Au-Yeung <leo297747@gmail.com>
2019-04-02 01:02:51 -0400

    Removed 0's from being displayed

--- gridDataDisplay.js
+++ gridDataDisplay.js
@@ -14,7 +14,13 @@ class GridDataDisplay {
             res += "<div class='row'>";
             for (let col = 0; col < size; col++) {
                 let cell = new Cell(row, col);
-                res += "<div>" + data.getCellValue(cell) + "</div>";
+                // If cell if a 0 (empty), draw whitespace (&nbsp;) on board
+                if (data.getCellValue(cell) == 0) {
+                    res += "<div>&nbsp;</div>";
+                }
+                else {
+                    res += "<div>" + data.getCellValue(cell) + "</div>";
+                }
             }
             res += "</div>";
         }
